b323f060e66519825564f738ffdf83839a8f6109
braxton1231121 <118190987+braxton1231121@users.noreply.github.com>
2022-11-18 09:11:17 -0500

    gvyefe8due8dhwhf

--- main.ts
+++ main.ts
@@ -23,4 +23,140 @@ basic.forever(function () {
         . . . . .
         . . . . .
         `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . # #
+        . # # # .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("clock")
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # # # # #
+        # . # . #
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("lamp")
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("tree")
+    basic.pause(100)
+    basic.showLeds(`
+        # . . . .
+        # # # # .
+        # # # # .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("flag")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("pyrimid")
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("house")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("smiley face ")
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # # # #
+        # # # # #
+        # . # . #
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("ghost")
+    basic.pause(100)
 })
